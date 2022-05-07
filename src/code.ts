7a979880b8ccb7b419e33bc92c5db49457a23919
aaroniker <aaron@column.com>
2022-05-07 11:19:06 +0200

    update dependencies, add react

--- src/code.ts
+++ src/code.ts
@@ -1,137 +1,132 @@
-const {skewDEG, rotateDEG, compose} = require('transformation-matrix')
+const { skewDEG, rotateDEG, compose } = require("transformation-matrix");
 
-let selection = figma.currentPage.selection
+let selection = figma.currentPage.selection;
 
-if(selection.length !== 1) {
-    figma.closePlugin('Select a single node.')
+if (selection.length !== 1) {
+  figma.closePlugin("Select a single node.");
 }
 
 function getOptions(direction) {
-    switch(direction) {
-        case 'left':
-            return {
-                rotate: 0,
-                skew: 30,
-                degree: -30
-            }
-        break
-        case 'right':
-            return {
-                rotate: 0,
-                skew: -30,
-                degree: 30
-            }
-        break
-        case 'top-left':
-            return {
-                rotate: 0,
-                skew: -30,
-                degree: -30
-            }
-        break
-        case 'top-right':
-            return {
-                rotate: 90,
-                skew: -30,
-                degree: 30
-            }
-        break
-        default:
-            return {
-                rotate: 0,
-                skew: 0,
-                degree: 0
-            }
-        break
-    }
+  switch (direction) {
+    case "left":
+      return {
+        rotate: 0,
+        skew: 30,
+        degree: -30,
+      };
+      break;
+    case "right":
+      return {
+        rotate: 0,
+        skew: -30,
+        degree: 30,
+      };
+      break;
+    case "top-left":
+      return {
+        rotate: 0,
+        skew: -30,
+        degree: -30,
+      };
+      break;
+    case "top-right":
+      return {
+        rotate: 90,
+        skew: -30,
+        degree: 30,
+      };
+      break;
+    default:
+      return {
+        rotate: 0,
+        skew: 0,
+        degree: 0,
+      };
+      break;
+  }
 }
 
 function setIsomentric(node, direction) {
+  let options = getOptions(direction),
+    matrix = compose(rotateDEG(options.rotate), skewDEG(0, options.skew)),
+    x = node.x,
+    y = node.y;
 
-    let options = getOptions(direction),
-        matrix = compose(
-            rotateDEG(options.rotate),
-            skewDEG(0, options.skew)
-        ),
-        x = node.x,
-        y = node.y
+  node.relativeTransform = [
+    [matrix.a, matrix.b, matrix.e],
+    [matrix.c, matrix.d, matrix.f],
+  ];
+  node.rotation = options.degree;
 
-    node.relativeTransform = [
-        [matrix.a, matrix.b, matrix.e],
-        [matrix.c, matrix.d, matrix.f]
-    ]
-    node.rotation = options.degree
+  node.x = x;
+  node.y = y;
 
-    node.x = x
-    node.y = y
+  node.setPluginData("direction", direction);
 
-    node.setPluginData('direction', direction)
-
-    return node
-
+  return node;
 }
 
 function setActive(selection) {
-    if(selection.length !== 1) {
-        return false
-    }
-    return selection[0].getPluginData('direction') || false
+  if (selection.length !== 1) {
+    return false;
+  }
+  return selection[0].getPluginData("direction") || false;
 }
 
-if(figma.command == 'modal') {
+if (figma.command == "modal") {
+  figma.showUI(__html__, {
+    width: 320,
+    height: 328,
+  });
 
-    figma.showUI(__html__, {
-        width: 320,
-        height: 328
-    })
-
-    figma.on('selectionchange', () => {
-        selection = figma.currentPage.selection
-        if(selection.length !== 1) {
-            figma.closePlugin('Select a single node.')
-        }
-        figma.ui.postMessage({
-            type: 'setActive',
-            active: setActive(selection)
-        })
-    })
-
+  figma.on("selectionchange", () => {
+    selection = figma.currentPage.selection;
+    if (selection.length !== 1) {
+      figma.closePlugin("Select a single node.");
+    }
     figma.ui.postMessage({
-        type: 'setActive',
-        active: setActive(selection)
-    })
+      type: "setActive",
+      active: setActive(selection),
+    });
+  });
 
-    figma.clientStorage.getAsync('easometricClose').then(bool => {
-        bool = bool === undefined ? true : bool
-        figma.ui.postMessage({
-            type: 'setToggle',
-            bool: bool
-        })
-    })
+  figma.ui.postMessage({
+    type: "setActive",
+    active: setActive(selection),
+  });
 
-    figma.ui.onmessage = response => {
+  figma.clientStorage.getAsync("easometricClose").then((bool) => {
+    bool = bool === undefined ? true : bool;
+    figma.ui.postMessage({
+      type: "setToggle",
+      bool: bool,
+    });
+  });
 
-        if(response.type == 'set') {
-            figma.clientStorage.getAsync('easometricClose').then(bool => {
-                bool = bool === undefined ? true : bool
-                setIsomentric(selection[0], response.direction)
-                if(bool) {
-                    figma.closePlugin('Isometric set.')
-                }
-            })
+  figma.ui.onmessage = (response) => {
+    if (response.type == "set") {
+      figma.clientStorage.getAsync("easometricClose").then((bool) => {
+        bool = bool === undefined ? true : bool;
+        setIsomentric(selection[0], response.direction);
+        if (bool) {
+          figma.closePlugin("Isometric set.");
         }
-
-        if(response.type == 'toggle') {
-            figma.clientStorage.setAsync('easometricClose', response.bool).then(() => {
-                figma.notify(response.bool ? 'Modal will close after selection.' : 'Modal will stay after selection.')
-            })
-        }
-
+      });
     }
 
+    if (response.type == "toggle") {
+      figma.clientStorage
+        .setAsync("easometricClose", response.bool)
+        .then(() => {
+          figma.notify(
+            response.bool
+              ? "Modal will close after selection."
+              : "Modal will stay after selection."
+          );
+        });
+    }
+  };
 } else {
-
-    setIsomentric(selection[0], figma.command)
-    figma.closePlugin('Isometric set.')
-
+  setIsomentric(selection[0], figma.command);
+  figma.closePlugin("Isometric set.");
 }
---
+++ src/ui.tsx
@@ -0,0 +1,185 @@
+import React, { useEffect, ChangeEvent, useState } from "react";
+import ReactDOM from "react-dom";
+import "./ui.scss";
+
+export const App: React.FC = () => {
+  const [direction, setDirection] = useState<string>("");
+  const [directionHover, setDirectionHover] = useState<string>("");
+  const [checked, setChecked] = useState<boolean>(false);
+
+  const handleClick = (d: string) => {
+    const newDirection = d === direction ? "none" : d;
+
+    setDirection(newDirection);
+
+    parent.postMessage(
+      {
+        pluginMessage: {
+          type: "set",
+          direction: newDirection,
+        },
+      },
+      "*"
+    );
+  };
+
+  const handleEnter = (d: string) => {
+    setDirectionHover(d);
+  };
+
+  const handleLeave = () => {
+    setDirectionHover("");
+  };
+
+  useEffect(() => {
+    window.onmessage = async (event) => {
+      if (event.data.pluginMessage.type == "setToggle") {
+        setChecked(event.data.pluginMessage.bool);
+      }
+      if (event.data.pluginMessage.type == "setActive") {
+        if (event.data.pluginMessage.active) {
+          setDirection(event.data.pluginMessage.active);
+        }
+      }
+    };
+  }, []);
+
+  return (
+    <div id="easometric">
+      <div className="box">
+        <div
+          className={`left ${direction === "left" && "active"} ${
+            directionHover === "left" && "hover"
+          }`}
+          data-direction="left"
+          onClick={() => handleClick("left")}
+          onMouseEnter={() => handleEnter("left")}
+          onMouseLeave={handleLeave}
+        ></div>
+        <div className="top">
+          <span
+            className={`${direction === "top-left" && "active"} ${
+              directionHover === "top-left" && "hover"
+            }`}
+            data-direction="top-left"
+            onClick={() => handleClick("top-left")}
+            onMouseEnter={() => handleEnter("top-left")}
+            onMouseLeave={handleLeave}
+          >
+            <svg viewBox="0 0 24 24">
+              <path
+                fill="currentColor"
+                d="M18.77 13.36a1 1 0 0 0-1.41-.13L13 16.86V5a1 1 0 0 0-2 0v11.86l-4.36-3.63a1 1 0 1 0-1.28 1.54l6 5 .15.09.13.07a1 1 0 0 0 .72 0l.13-.07.15-.09 6-5a1 1 0 0 0 .13-1.41z"
+              ></path>
+            </svg>
+          </span>
+          <span
+            className={`${direction === "top-right" && "active"} ${
+              directionHover === "top-right" && "hover"
+            }`}
+            data-direction="top-right"
+            onClick={() => handleClick("top-right")}
+            onMouseEnter={() => handleEnter("top-right")}
+            onMouseLeave={handleLeave}
+          >
+            <svg viewBox="0 0 24 24">
+              <path
+                fill="currentColor"
+                d="M18.77 13.36a1 1 0 0 0-1.41-.13L13 16.86V5a1 1 0 0 0-2 0v11.86l-4.36-3.63a1 1 0 1 0-1.28 1.54l6 5 .15.09.13.07a1 1 0 0 0 .72 0l.13-.07.15-.09 6-5a1 1 0 0 0 .13-1.41z"
+              ></path>
+            </svg>
+          </span>
+        </div>
+        <div
+          className={`right ${direction === "right" && "active"} ${
+            directionHover === "right" && "hover"
+          }`}
+          data-direction="right"
+          onClick={() => handleClick("right")}
+          onMouseEnter={() => handleEnter("right")}
+          onMouseLeave={handleLeave}
+        ></div>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <button
+              className={`left ${direction === "left" && "active"} ${
+                directionHover === "left" && "hover"
+              }`}
+              data-direction="left"
+              onClick={() => handleClick("left")}
+              onMouseEnter={() => handleEnter("left")}
+              onMouseLeave={handleLeave}
+            >
+              Left
+            </button>
+          </li>
+          <li>
+            <button
+              className={`${direction === "top-left" && "active"} ${
+                directionHover === "top-left" && "hover"
+              }`}
+              data-direction="top-left"
+              onClick={() => handleClick("top-left")}
+              onMouseEnter={() => handleEnter("top-left")}
+              onMouseLeave={handleLeave}
+            >
+              Top L
+            </button>
+          </li>
+          <li>
+            <button
+              className={`${direction === "top-right" && "active"} ${
+                directionHover === "top-right" && "hover"
+              }`}
+              data-direction="top-right"
+              onClick={() => handleClick("top-right")}
+              onMouseEnter={() => handleEnter("top-right")}
+              onMouseLeave={handleLeave}
+            >
+              Top R
+            </button>
+          </li>
+          <li>
+            <button
+              className={`right ${direction === "right" && "active"} ${
+                directionHover === "right" && "hover"
+              }`}
+              data-direction="right"
+              onClick={() => handleClick("right")}
+              onMouseEnter={() => handleEnter("right")}
+              onMouseLeave={handleLeave}
+            >
+              Right
+            </button>
+          </li>
+        </ul>
+      </nav>
+      <label className="switch">
+        <span>Close after selection</span>
+        <input
+          type="checkbox"
+          name="close"
+          checked={checked}
+          onChange={(event: ChangeEvent<HTMLInputElement>) => {
+            setChecked(event.target.checked);
+
+            parent.postMessage(
+              {
+                pluginMessage: {
+                  type: "toggle",
+                  bool: event.target.checked,
+                },
+              },
+              "*"
+            );
+          }}
+        />
+        <div></div>
+      </label>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("react-page"));
